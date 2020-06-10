e6b30d63abc2143976ed3064054949627e9c55aa
wellDMLT <you@example.comwellington.acardoso18@gmail.com>
2020-06-10 15:00:54 -0300

    desenvolvendo o post

---
+++ labeddit/src/pages/PostPage/PostPage.js
@@ -0,0 +1,76 @@
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import styled from 'styled-components'
+
+import axios from 'axios'
+
+const Post = styled.div`
+  width: 50%;
+  height: 50%;
+  margin: 0 auto;
+  border-radius: 8px;
+  border: 1px solid #c3c3c3;
+  box-shadow: 0 2px 7px 0 rgba(0,0,0,0.15);
+  display: flex;
+  padding: 15px;
+  text-align: center;
+  
+`
+const baseUrl = 'https://us-central1-labenu-apis.cloudfunctions.net/labEddit'
+
+const PostPage = () => {
+    const HomePage = useHistory();
+    const FeedPage = useHistory(); 
+    /* const [getPost, setGetPost] = useState([]) */
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+    
+        if(token === null){
+          HomePage.push("/")
+        }
+      },[HomePage])
+
+    const getPost = async (pros) =>{
+      try{
+        const response = await axios.get(`${baseUrl}/:id`,{
+          header:{
+            auth: props.id
+          }
+        })
+        localStorage.getItem('token', response.data.token)
+        console.log(response)
+      }
+      catch(e){
+        console.log('deu ruim')
+      }
+    }
+    
+
+    const goToHomePage = () => {
+        HomePage.push("/")
+    }
+    const goToFeedPage = () => {
+    FeedPage.push("/feed-page/")
+    }  
+
+    return (    
+        <div>
+            <h2>Página do Post</h2>
+            <Post>
+                <div>
+                Post
+                </div>
+                
+                <div>
+                  <br></br><br></br>
+                    <button onClick={goToFeedPage}>Voltar para Página dos Feeds</button>                
+                </div>                    
+            </Post>
+            <button onClick={goToHomePage}>Sair</button>
+        </div>  
+
+  );
+}
+
+export default PostPage;
